151260a09576c0eaedae23ef5a271ef64b4b18d0
Mariam <mariam@localhost.localdomain>
2018-10-22 10:26:33 +0300

    created routes for a table which multiplies numbers from 1 to 10 with 2 and also another route which displays a table which multiples numbers from 1-10 with number itself

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/table', function () {
+
+   for($i =1; $i <= 10 ; $i++){
+
+       echo "$i * 2 = ". $i*2 ."<br>";
+
+   }
+
+});
+
+Route::get('/table/{number}', function ($number) {
+
+   for($i =1; $i <= 10 ; $i++){
+
+       echo "$i * $number = ". $i* $number ."<br>";
+
+   }
+
+});
